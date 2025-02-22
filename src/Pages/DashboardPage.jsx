e7faf60c1d779ed9d6a090e55706ea35703f0fed
busybiee1 <busybiee1@gmail.com>
2025-02-22 10:17:28 -0600

    dashboard and login functionality

---
+++ src/Pages/DashboardPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+//import Banner from '../components/Banner';
+import Navigation from '../components/Navigation';
+import Dashboard from '../components/Dashboard';
+
+function DashboardPage () {
+//const DashboardPage = () => {
+  return (
+    <div className="dashboard-page">
+      {/* <Banner /> */}
+      {/* <Navigation /> */}
+      {/* <h2>Dashboard</h2> */}
+      <Dashboard />
+    </div>
+  );
+};
+
+export default DashboardPage;
